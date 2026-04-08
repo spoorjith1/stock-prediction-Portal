d3c246dd0e14f4f4873536ece2e5ea66f79bef01
spoorjith1 <spoorjith1@gmail.com>
2026-04-08 19:23:34 +0530

    homepage

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
+import './assets/css/style.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
 
 function App() {
 
   return (
     <>
+    <Header />
+    <Main />
+    <Footer />
     </>
   )
 }
