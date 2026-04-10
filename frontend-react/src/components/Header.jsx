8e81b7d01f02baf2987075f633a865e2eba78461
spoorjith1 <spoorjith1@gmail.com>
2026-04-10 15:08:27 +0530

    authentication

--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Button from './Button'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../AuthProvider'
 
 const Header = () => {
+  const {isLoggedIn, setIsLoggedIn} = useContext(AuthContext)
+  const navigate = useNavigate()
+  
+  const handleLogout = ()=> {
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('refreshToken')
+    setIsLoggedIn(false)
+    navigate('/login')
+  }
+
   return (
     <>
     <nav className='navbar container pt-3 pb-3 align-items-start'>
         <Link to="/" className="navbar-brand text-light">Stock Prediction Portal</Link>
 
         <div>
-            <Button text='Login' class='btn-outline-info' url='/login' />
-            &nbsp;
-            <Button text='Register' class='btn-info' url='/register' />
+          {isLoggedIn ? (
+            <button className='btn btn-danger' onClick={handleLogout}>Logout</button>
+          ) : (
+            <>
+              <Button text='Login' class='btn-outline-info' url='/login' />
+              &nbsp;
+              <Button text='Register' class='btn-info' url='/register' />
+            </>
+          )}
         </div>
     </nav> 
     </>
--- frontend-react/src/components/Login.jsx
+++ frontend-react/src/components/Login.jsx
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../AuthProvider'
 
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+  const [error, setError] = useState('')
+  const {isLoggedIn, setIsLoggedIn} = useContext(AuthContext)
+
+  const handlelogin = async (e)=> {
+    e.preventDefault();
+    setLoading(true)
+
+    const userData = {username, password}
+
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/v1/token/', userData)
+      localStorage.setItem('accessToken', response.data.access)
+      localStorage.setItem('refreshToken', response.data.refresh)
+      setIsLoggedIn(true)
+      navigate('/')
+    }
+    catch (error) {
+      setError('Invalid Credentials')
+    }
+    finally {
+      setLoading(false)
+    }
+  }
+
   return (
-    <div className=' text-light container'>
-      Login
+    <>
+    <div className='container'>
+      <div className='row justify-content-center'>
+        <div className='col-md-6 bg-light-dark p-5 rounded'>
+          <h3 className='text-light text-center mb-4'>Login to our Portal</h3>
+          <form onSubmit={handlelogin}>
+            <div className='mb-3'>
+              <input type='text' placeholder='username' className='form-control' value={username} 
+              onChange={(e)=> setUsername(e.target.value)} />
+            </div>
+            <div className='mb-3'>
+              <input type='password' placeholder='password' className='form-control' value={password} 
+              onChange={(e)=> setPassword(e.target.value)}/>
+            </div>
+            {error && <div className='text-danger'>{error}</div>}
+            {loading ? (
+              <button type='submit' className='btn btn-info d-block mx-auto' disabled><FontAwesomeIcon icon={faSpinner} spin/> Logging in...</button>
+            ) : (
+              <button type='submit' className='btn btn-info d-block mx-auto'>Login</button>
+            )}
+          </form>
+        </div>
+      </div>
     </div>
+    </>
   )
 }
 
